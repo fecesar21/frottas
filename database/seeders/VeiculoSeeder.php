fa6c025e691ff712a8c327670870a3bb2e629875
Fernando César Cardoso <fernandocesar.adm@gmail.com>
2026-07-09 21:09:47 -0300

    fix: COLUNA CAPACIDADE VEICULO E COR

--- database/seeders/VeiculoSeeder.php
+++ database/seeders/VeiculoSeeder.php
@@ -18,7 +18,7 @@ class VeiculoSeeder extends Seeder
             ['placa' => 'RSC-8D13', 'modelo' => 'DUSTER', 'marca' => 'Renault', 'ano' => 2022, 'cor' => 'BRANCO', 'combustivel' => 'flex', 'capacidade_tanque' => '45', 'km_atual' => 1],
             ['placa' => 'RT0-9J21', 'modelo' => 'AMBULÂNCIA UPA', 'marca' => 'Mercedes-Benz', 'ano' => 2019, 'cor' => 'BRANCO', 'combustivel' => 'diesel_s10', 'capacidade_tanque' => '45', 'km_atual' => 1],
             ['placa' => 'SDO-5B00', 'modelo' => 'COROLLA', 'marca' => 'Toyota', 'ano' => 2023, 'cor' => 'BRANCO', 'combustivel' => 'flex', 'capacidade_tanque' => '45', 'km_atual' => 1],
-            ['placa' => 'TVD-7F43', 'modelo' => 'Fiorino', 'marca' => 'Fiat', 'ano' => 2020, 'cor' => 'BRANCO', 'combustivel' => 'flex', 'capacidade_tanque' => '', 'km_atual' => 1],
+            ['placa' => 'TVD-7F43', 'modelo' => 'Fiorino', 'marca' => 'Fiat', 'ano' => 2020, 'cor' => 'BRANCO', 'combustivel' => 'flex', 'capacidade_tanque' => '45', 'km_atual' => 1],
         ];
 
         foreach ($veiculos as $dados) {
